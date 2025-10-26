d542605c6b72bcfe2a3bcb3eaa2ee00f95b0a809
kyran <kyran_wolking@yahoo.com>
2025-10-26 04:47:47 +0100

    Product class

---
+++ VeilingKlokKlas1Groep2/Models/OutputDTOs/ProductDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace VeilingKlokApp.Models;
+
+public class ProductDetails
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public decimal MinimumPrice { get; set; }
+    public int Quantity { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Size { get; set; }
+    public int KwekerId { get; set; }
+    public string? KwekerName { get; set; }
+}
