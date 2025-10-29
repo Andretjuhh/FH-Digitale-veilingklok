dece604ca6d78de98295beeacc389bd3416d5b67
Andretjuhh <andre.silvareis12@gmail.com>
2025-10-29 10:53:43 +0100

    Dashboard werkt nu

--- VeilingKlokKlas1Groep2/Application/src/App.tsx
+++ VeilingKlokKlas1Groep2/Application/src/App.tsx
@@ -1,18 +1,20 @@
 // External imports
-import React from 'react';
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
 // Internal imports
 import Home from "./pages/Home";
-import './styles/app.css';
+import Dashboard from "./pages/Dashboard";
+import "./styles/app.css";
 
 function App() {
-	const location = useLocation();
+  const location = useLocation();
 
-	return (
-		<Routes location={location} key={location.pathname}>
-			<Route path="/" element={<Home />} />
-		</Routes>
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<Home />} />
+      <Route path="/dash" element={<Dashboard />} />
+    </Routes>
   );
 }
 
