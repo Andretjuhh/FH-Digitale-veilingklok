90b1013dea2a9ed65e7967793e2223db51d418a5
SebEmmen <sebastiaanemmen@gmail.com>
2025-10-29 10:56:00 +0100

    Merged RLA completely, and added navigation
    There is now navigation from home page to login and registration page. Also added a very simple back button to the login and registration page that brings you back to home

--- VeilingKlokKlas1Groep2/Application/src/App.tsx
+++ VeilingKlokKlas1Groep2/Application/src/App.tsx
@@ -1,25 +1,26 @@
 // External imports
 import React from 'react';
-import {Routes, Route, useLocation} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 // Internal imports
 import RootContext from './contexts/RootContext';
-import Home from "./pages/general/Home";
-import Login from "./pages/general/Login";
-import Register from "./pages/general/Register";
+import Home from './pages/general/Home';
+import Login from './pages/general/Login';
+import Register from './pages/general/Register';
 import './styles/app.css';
 
 function App() {
 	const location = useLocation();
 
 	return (
-		<Routes location={location} key={location.pathname}>
-			<Route path="/" element={<Home />} />
-			<Route path="/login" element={<Login />} />
-			<Route path='/register' element={<Register />} />
-		</Routes>
-  );
+		<RootContext>
+			<Routes location={location} key={location.pathname}>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</RootContext>
+	);
 }
-
 
 export default App;
