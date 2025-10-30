519e623c9ff27d679a728f7a3dccf74940059531
ImRoodyDev <roodyalexis36@gmail.com>
2025-10-30 11:27:29 +0100

    Reapply "fix: header styling"
    This reverts commit 6cf3f4a8c4d89229079d7d53f053b5963683a9f9.

--- VeilingKlokKlas1Groep2/Application/src/components/nav/AppHeader.tsx
+++ VeilingKlokKlas1Groep2/Application/src/components/nav/AppHeader.tsx
@@ -1,26 +1,54 @@
 // External imports
-import React from 'react';
- 
+import React, { useEffect, useState } from 'react';
+
 // Internal imports
-import LanguagePicker from "../buttons/LanguagePicker";
+import LanguagePicker from '../buttons/LanguagePicker';
+import clsx from 'clsx';
 
 function AppHeader() {
+	const [scrollScale, setScrollScale] = useState(0);
+	const [isFilled, setIsFilled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollTop = window.scrollY;
+			// Animate the ripple over the first 200px of scrolling
+			const scrollFraction = Math.min(scrollTop / 200, 1);
+			setScrollScale(scrollFraction);
+			setIsFilled(scrollFraction === 1);
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
-		<header className="app-header">
+		<header className={clsx('app-header', isFilled && 'app-header-filled')} style={{ '--scroll-scale': scrollScale } as React.CSSProperties}>
 			<div className={'header-logo'}>
-				<img className={'header-logo-img'} src={'/svg/logo-floriclock.svg'} alt={'FloriClock'}/>
+				<img className={'header-logo-img'} src={'/svg/logo-floriclock.svg'} alt={'FloriClock'} />
 			</div>
 
 			<nav className="header-nav">
 				<ul className={'nav-menu'}>
-					<a className={'nav-menu-anchor'} href={"#what-is-flori-clock"}>{window.application.t('what_is_flori_clock')}</a>
-					<a className={'nav-menu-anchor'} href={"#soort-bloemen"}>{window.application.t('flower_types')}</a>
-					<a className={'nav-menu-anchor'} href={"#what-is-flori-clock"}>{window.application.t('how_it_works')}</a>
-					<a className={'nav-menu-anchor'} href={"#what-is-flori-clock"}>{window.application.t('contact_us')}</a>
+					<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
+						{window.application.t('what_is_flori_clock')}
+					</a>
+					<a className={'nav-menu-anchor'} href={'#soort-bloemen'}>
+						{window.application.t('flower_types')}
+					</a>
+					<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
+						{window.application.t('how_it_works')}
+					</a>
+					<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
+						{window.application.t('contact_us')}
+					</a>
 				</ul>
 			</nav>
 
-			<LanguagePicker/>
+			<LanguagePicker />
 		</header>
 	);
 }
