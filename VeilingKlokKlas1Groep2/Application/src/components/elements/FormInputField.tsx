42e51ed508f9c53912cf7daa69806414e4e1ee9c
ImRoodyDev <roodyalexis36@gmail.com>
2025-11-11 14:08:06 +0100

    fix: cleanup

--- VeilingKlokKlas1Groep2/Application/src/components/elements/FormInputField.tsx
+++ VeilingKlokKlas1Groep2/Application/src/components/elements/FormInputField.tsx
@@ -3,23 +3,25 @@ import {joinClsx} from "../../utils/classPrefixer";
 import clsx from "clsx";
 
 type Props = {
+	id: string;
 	label: string;
 	error: string;
 	isError: boolean;
 	className?: string;
 	mainClassName?: string;
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'className'>;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'className' | 'id'>;
 
 function FormInputField(props: Props) {
-	const {isError, label, error, mainClassName, className, ...inputProps} = props;
+	const {id, isError, label, error, mainClassName, className, ...inputProps} = props;
+
 	return (
 		<div className={clsx(mainClassName, className)}>
 			<label
-				htmlFor={inputProps.id}
+				htmlFor={id}
 				className={clsx("input-field-label", joinClsx(className, 'input'))}
 			>{label}
 			</label>
-			<input className={clsx("input-field")} {...inputProps}/>
+			<input id={id} className={clsx("input-field")} {...inputProps}/>
 			{
 				isError && (
 					<div className={clsx("input-field-error-feedback", joinClsx(className, 'error-feedback'))}>
--- VeilingKlokKlas1Groep2/Application/src/components/elements/TextAreaInputField.tsx
+++ VeilingKlokKlas1Groep2/Application/src/components/elements/TextAreaInputField.tsx
@@ -3,23 +3,25 @@ import {joinClsx} from "../../utils/classPrefixer";
 import clsx from "clsx";
 
 type Props = {
+	id: string;
 	label: string;
 	error: string;
 	isError: boolean;
 	className?: string;
 	mainClassName?: string;
-} & Omit<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'className'>;
+} & Omit<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'className' | 'id'>;
 
 function TextAreaInputField(props: Props) {
-	const {isError, label, error, mainClassName, className, ...inputProps} = props;
+	const {id, isError, label, error, mainClassName, className, ...inputProps} = props;
+
 	return (
 		<div className={clsx(mainClassName, className)}>
 			<label
-				htmlFor={inputProps.name}
+				htmlFor={id}
 				className={clsx("input-field-label", joinClsx(className, 'input'))}
 			>{label}
 			</label>
-			<textarea className={clsx("input-field")} {...inputProps}/>
+			<textarea id={id} className={clsx("input-field")} {...inputProps}/>
 			{
 				isError && (
 					<div className={clsx("input-field-error-feedback", joinClsx(className, 'error-feedback'))}>
