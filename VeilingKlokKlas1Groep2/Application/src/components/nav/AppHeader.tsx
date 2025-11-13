0d8b7ff6f7d6d1add5ca251fb5adbfafea4fb78e
ImRoodyDev <roodyalexis36@gmail.com>
2025-11-13 12:34:14 +0100

    added new auth system

--- VeilingKlokKlas1Groep2/Application/src/components/nav/AppHeader.tsx
+++ VeilingKlokKlas1Groep2/Application/src/components/nav/AppHeader.tsx
@@ -1,15 +1,17 @@
 // External imports
-import React, {useCallback, useEffect, useState} from 'react';
-import {useLocation, useNavigate} from "react-router-dom";
+import React, { useCallback, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 // Internal imports
 import LanguagePicker from '../buttons/LanguagePicker';
 import clsx from 'clsx';
-import Button from "../buttons/Button";
+import Button from '../buttons/Button';
+import { useRootContext } from '../../contexts/RootContext';
 
 function AppHeader() {
 	let location = useLocation();
 	const navigate = useNavigate();
+	const { removeAuthentication } = useRootContext();
 
 	const [scrollScale, setScrollScale] = useState(0);
 	const [isFilled, setIsFilled] = useState(false);
@@ -32,63 +34,56 @@ function AppHeader() {
 
 	const handleLogout = useCallback(() => {
 		navigate('/');
-	}, [])
+		removeAuthentication();
+	}, []);
 
-	console.log(location.pathname)
+	console.log(location.pathname);
 
 	return (
 		<>
-			<header className={clsx('app-header', isFilled && 'app-header-filled')} style={{'--scroll-scale': scrollScale} as React.CSSProperties}>
+			<header className={clsx('app-header', isFilled && 'app-header-filled')} style={{ '--scroll-scale': scrollScale } as React.CSSProperties}>
 				<div className={'header-logo'}>
-					<img className={'header-logo-img'} src={'/svg/logo-floriclock.svg'} alt={'FloriClock'}/>
+					<img className={'header-logo-img'} src={'/svg/logo-floriclock.svg'} alt={'FloriClock'} />
 				</div>
 				<nav className="header-nav">
 					<ul className={'nav-menu'}>
-						{
-							['', '/'].includes(location.pathname) ?
-								<>
-									<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
-										{window.application.t('what_is_flori_clock')}
-									</a>
-									<a className={'nav-menu-anchor'} href={'#soort-bloemen'}>
-										{window.application.t('flower_types')}
-									</a>
-									<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
-										{window.application.t('how_it_works')}
-									</a>
-									<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
-										{window.application.t('contact_us')}
-									</a>
-								</>
-								:
-								<>
-									<a className={'nav-menu-anchor'} href={'#'}>
-										{window.application.t('dashboard')}
-									</a>
-									<a className={'nav-menu-anchor'} href={'#'}>
-										{window.application.t('orders')}
-									</a>
-									<a className={'nav-menu-anchor'} href={'#'}>
-										{window.application.t('manage_account')}
-									</a>
-									<a className={'nav-menu-anchor'} href={'#'}>
-										{window.application.t('settings')}
-									</a>
-								</>
-						}
-
+						{['', '/'].includes(location.pathname) ? (
+							<>
+								<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
+									{window.application.t('what_is_flori_clock')}
+								</a>
+								<a className={'nav-menu-anchor'} href={'#soort-bloemen'}>
+									{window.application.t('flower_types')}
+								</a>
+								<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
+									{window.application.t('how_it_works')}
+								</a>
+								<a className={'nav-menu-anchor'} href={'#what-is-flori-clock'}>
+									{window.application.t('contact_us')}
+								</a>
+							</>
+						) : (
+							<>
+								<a className={'nav-menu-anchor'} href={'#'}>
+									{window.application.t('dashboard')}
+								</a>
+								<a className={'nav-menu-anchor'} href={'#'}>
+									{window.application.t('orders')}
+								</a>
+								<a className={'nav-menu-anchor'} href={'#'}>
+									{window.application.t('manage_account')}
+								</a>
+								<a className={'nav-menu-anchor'} href={'#'}>
+									{window.application.t('settings')}
+								</a>
+							</>
+						)}
 					</ul>
 				</nav>
-				{
-					!['', '/'].includes(location.pathname) &&
-					<Button className={'app-home-s-btn app-header-logout'} label={window.application.t('logout')} onClick={handleLogout}/>
-				}
+				{!['', '/'].includes(location.pathname) && <Button className={'app-home-s-btn app-header-logout'} label={window.application.t('logout')} onClick={handleLogout} />}
 			</header>
 
-			{
-				['', '/'].includes(location.pathname) &&
-				<LanguagePicker/>
-			}
+			{['', '/'].includes(location.pathname) && <LanguagePicker />}
 		</>
 	);
 }
--- VeilingKlokKlas1Groep2/Application/src/constant/application.ts
+++ VeilingKlokKlas1Groep2/Application/src/constant/application.ts
@@ -1,6 +1,6 @@
 const config = {
 	VERSION: '1.0.0',
-	API: 'http://localhost:5050/api/',
+	API: 'http://localhost:5050/',
 } as const;
 
 export default config;
