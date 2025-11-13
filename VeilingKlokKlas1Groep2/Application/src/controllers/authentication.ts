00d7cd325a525daef1fb6d02777529846a55f203
ImRoodyDev <roodyalexis36@gmail.com>
2025-11-13 02:51:40 +0100

    fix: new routes

--- VeilingKlokKlas1Groep2/Application/src/controllers/authentication.ts
+++ VeilingKlokKlas1Groep2/Application/src/controllers/authentication.ts
@@ -1,42 +1,9 @@
 import { fetchResponse } from '../utils/fetchHelpers';
 import { LoginRequest } from '../declarations/LoginRequest';
-import { AuthResponse } from '../declarations/AuthenticationResponse';
-import { AccountInfo } from '../declarations/AccountInfo';
 
-// Initialize authentication by chekcing if tokens are valid
-export async function initializeAuthentication() {
-	try {
-		const response = await fetchResponse<AccountInfo>('/api/auth/account', {
-			method: 'GET',
-		});
-		window.application.account = response;
-		return response;
-	} catch {
-		localStorage.removeItem('accessToken');
-		return undefined;
-	}
-}
-
-// Login into account
 export async function loginAccount(account: LoginRequest) {
-	const response = await fetchResponse<AuthResponse>('/api/auth/login', {
+	return await fetchResponse('/api/auth/login', {
 		method: 'POST',
 		body: JSON.stringify(account),
 	});
-	await saveAuthenticationResponse(response);
-	return response;
-}
-
-// Logout from account
-export async function logoutAccount() {
-	await fetchResponse('/api/auth/logout', {
-		method: 'GET',
-	}).then(() => {
-		localStorage.removeItem('accessToken');
-	});
-}
-
-// Save the accesstoken in local storage
-export async function saveAuthenticationResponse(response: AuthResponse) {
-	localStorage.setItem('accessToken', response.accessToken);
 }
