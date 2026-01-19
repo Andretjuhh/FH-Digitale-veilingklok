be7abc165add8020929ab0aaebc94ac1c9f7e377
ImRoodyDev <roodyalexis36@gmail.com>
2026-01-19 23:46:53 +0100

    Refactor veiling klok status handler and update ProductCard
    Refactored UpdateVeilingKlokStatusHandler to improve transaction handling, status checks, and error handling for veiling klok status updates. Updated ProductCard.tsx to adjust badge display logic and apply consistent code formatting.

--- FrontendWeb/src/components/cards/ProductCard.tsx
+++ FrontendWeb/src/components/cards/ProductCard.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react';
-import { useRootContext } from '../contexts/RootContext';
-import { ProductOutputDto } from '../../declarations/dtos/output/ProductOutputDto';
-import CustomDropdown, { DropdownItem } from '../buttons/Dropdown';
-import { formatEur } from '../../utils/standards';
+import React, {useCallback} from 'react';
+import {useRootContext} from '../contexts/RootContext';
+import {ProductOutputDto} from '../../declarations/dtos/output/ProductOutputDto';
+import CustomDropdown, {DropdownItem} from '../buttons/Dropdown';
+import {formatEur} from '../../utils/standards';
 import clsx from 'clsx';
 
 export type ProductActions = 'edit' | 'delete' | 'set_pricing' | 'prijs_history';
@@ -16,16 +16,16 @@ type ProductCardProps = {
 };
 
 function ProductCard(props: ProductCardProps) {
-	const { product, index, mode = 'normal', isKoper = false } = props;
-	const { t } = useRootContext();
+	const {product, index, mode = 'normal', isKoper = false} = props;
+	const {t} = useRootContext();
 
 	const normalMenu: DropdownItem<ProductActions>[] = [
-		{ id: 'edit', label: t('edit_product'), type: 'button', as: 'button', icon: 'bi bi-pencil-fill' },
-		{ id: 'delete', label: t('delete_product'), type: 'button', as: 'button', icon: 'bi bi-trash3-fill' },
+		{id: 'edit', label: t('edit_product'), type: 'button', as: 'button', icon: 'bi bi-pencil-fill'},
+		{id: 'delete', label: t('delete_product'), type: 'button', as: 'button', icon: 'bi bi-trash3-fill'},
 	];
-	const meesterMenu: DropdownItem<ProductActions>[] = [{ id: 'set_pricing', label: t('set_auction_product_price'), type: 'button', as: 'button', icon: 'bi bi-euro' }];
+	const meesterMenu: DropdownItem<ProductActions>[] = [{id: 'set_pricing', label: t('set_auction_product_price'), type: 'button', as: 'button', icon: 'bi bi-euro'}];
 
-	const koperMenu: DropdownItem<ProductActions>[] = [{ id: 'prijs_history', label: t('koper_footer_history'), type: 'button', as: 'button', icon: 'bi bi-euro' }];
+	const koperMenu: DropdownItem<ProductActions>[] = [{id: 'prijs_history', label: t('koper_footer_history'), type: 'button', as: 'button', icon: 'bi bi-euro'}];
 
 	const handleSelect = useCallback(
 		(type: ProductActions) => {
@@ -36,12 +36,13 @@ function ProductCard(props: ProductCardProps) {
 
 	return (
 		<div className="product-card">
-			<div className="product-card__shine" />
-			<div className="product-card__glow" />
+			<div className="product-card__shine"/>
+			<div className="product-card__glow"/>
 			<div className="product-card__content">
-				{mode == 'meester' && <div className={clsx('product-card__badge !opacity-100 !scale-100 !z-10', product.auctionPlanned ? 'bg-green-500' : 'bg-yellow-400')}>{product.auctionPlanned ? t('scheduled_for_auction') : t('no_veiling_planned')}</div>}
+				{(mode == 'meester' || mode == 'normal') && <div
+					className={clsx('product-card__badge !opacity-100 !scale-100 !z-10', product.auctionPlanned ? 'bg-green-500' : 'bg-yellow-400')}>{product.auctionPlanned ? t('scheduled_for_auction') : t('no_veiling_planned')}</div>}
 				<div className="product-card__image">
-					<img src="/pictures/flower-test.avif" alt={t('alt_flower_picture')} />
+					<img src="/pictures/flower-test.avif" alt={t('alt_flower_picture')}/>
 				</div>
 				<div className="product-card__text">
 					<p className="product-card__title">{product.name}</p>
@@ -82,7 +83,7 @@ function ProductCard(props: ProductCardProps) {
 							itemButtonClassName={'product-card-dropdown-item-btn'}
 							buttonChildren={<i className="bi bi-three-dots-vertical"></i>}
 							items={mode == 'meester' ? meesterMenu : isKoper ? koperMenu : normalMenu}
-							onItemSelect={({ id }) => handleSelect(id)}
+							onItemSelect={({id}) => handleSelect(id)}
 						/>
 					)}
 				</div>
